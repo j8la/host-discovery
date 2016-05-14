9bcb6d8346360f0fc14b7d668e8e399e8cde98bd
Julien Blanc <Julien Blanc>
2016-05-14 03:08:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+host-discovery
+=============
+A simple service which sends messages to a multicast address in JSON format. It is designed to run on multiple hosts to maintain a list of subscribers to the multicast group. That can be useful for creating a distributed service. A filter, represented by the "service" field, can be used to filter messages if other hosts emit packets on the same multicast address. IPv4 and IPv6 are supported.
+
+## Installation
+```
+npm install host-discovery
+```
+
+## How to
+
+### Import module
+```javascript
+var hostDiscovery = require('host-discovery');
+```
+
+### Create instance
+```javascript
+var service = new hostDiscovery({
+    service: 'test',    // Default to 'all'
+    protocol: 'udp4',   // Default to udp4, can be udp6
+    port: 60600         // Default to 2900
+});
+```
+
+If default options are sufficient for you, you can create an instance like this :
+```javascript
+var service = new hostDiscovery();
+```
+
+Or for one option :
+```javascript
+var service = new hostDiscovery({
+    protocol: 'udp6'    
+});
+```
+
+### Events
+Two events are implemented :
+- `join` for new members of multicast group and service field
+- `leave` for members which has left multicast group
+
+```javascript
+service.on('join', (ip) => { 
+    console.log('A new member has joined the group : ' + ip); 
+});
+
+service.on('leave', (ip) => { 
+    console.log('A member has left the group : ' + ip); 
+});
+```
+
+The detection of a leaving host is between 10 and 15 seconds.
+
+### Methods
+To start the service :
+```javascript
+service.start();
+```
+
+To stop the service :
+```javascript
+service.stop();
+```
+
+To get active hosts :
+```javascript
+service.hosts();
+
+// The result will be :
+[ '192.168.0.11': { timestamp: 1463186992997, hostname: 'test01' },
+  '192.168.0.12': { timestamp: 1463186993629, hostname: 'test02' },
+  '192.168.0.13': { timestamp: 1463186993640, hostname: 'test03' } ]
+```
+
+## Updates
+- `v1.0.0 :` Initial release
+
+## Licence
+The MIT License (MIT) 
+Copyright (c) 2016 Julien Blanc
---
+++ host-discovery.js
@@ -0,0 +1,175 @@
+/*
+Name    : host-discovery.js
+Author  : Julien Blanc
+Version : 1.0.0
+Date    : 14/05/2016
+NodeJS  : 6.1.0+ 
+*/
+
+
+//----------------------------------------- LOAD MODULES
+var dgram       = require('dgram');
+var eventTimer  = require('event-timer');
+var event       = require('events').EventEmitter;
+
+
+//----------------------------------------- GLOBALS
+const timer = new eventTimer({
+    interval: 5000,
+    events:['tick']
+});
+
+var hosts       = [];
+var port        = null;
+var protocol    = null;
+var service     = null;
+
+
+//----------------------------------------- FUNCTIONS
+function init(self) {
+    
+    //------ Default multicast address is IPv4
+    var multicastAddr = '224.0.0.1';
+    
+    
+    //------ Creates socket
+    self.socket = dgram.createSocket({
+        type: protocol,
+        reuseAddr: true
+    });
+    
+    
+    //------ If IPv6, send to IPv6 multicast address
+    if(self.socket.type == 'udp6') {
+        multicastAddr = 'ff02::1';
+    }
+    
+    
+    //------ Listening
+    self.socket.on('listening', () => {
+        self.socket.addMembership(multicastAddr);
+    }); 
+    
+    
+    //------ Receives packet
+    self.socket.on('message', (message, rinfo) => {
+        
+        try {       
+            
+            var msg = JSON.parse(message);
+            var isNew = true;
+        
+            if(msg.service == service) {
+                
+                // If the host is sending a message for the first time, an event 
+                // occurs with his address
+                if(hosts.hasOwnProperty(rinfo.address)) {
+                    isNew = false;   
+                }
+                
+                // Creates or overrides the host item with reception time 
+                hosts[rinfo.address] = {
+                    timestamp: Date.now(),
+                    hostname: msg.host
+                }
+                
+                if(isNew == true) { self.emit('join', rinfo.address); }
+
+            }
+            
+        } catch(e) {}
+       
+    });
+    
+    
+    //------ Timer which sends a packet to multicast address
+    timer.on('tick', () => {
+        
+        const notify = new Buffer([
+            '{',
+            '"service":"' + service + '",',
+            '"host":"' + os.hostname() + '"',
+            '}'
+        ].join('\r\n'));
+
+        self.socket.send(notify, 0, notify.length, port, multicastAddr);
+        
+        // If the difference between the current time and the arrival packet time 
+        // is greater than two times the timer interval, the member is removed 
+        // from list
+        for(host in hosts) {
+
+            if((Date.now() - hosts[host].timestamp) > (timer.interval*2)) {
+                delete hosts[host];
+                self.emit('leave', host);
+            }
+            
+        }
+        
+    });
+    
+}
+
+
+//----------------------------------------- CLASS
+
+//------ Constructor
+function jsd(options) {
+    
+    if(options == null) { options = {}; }
+        
+    protocol    = options.protocol  || 'udp4';
+    service     = options.service   || 'all';
+    port        = options.port      || 2900;
+    
+    if(service.length > 25 ) {
+        throw('\'service\' is limited to a size of 25 bytes.');
+    }
+    
+    if(protocol != 'udp4' && protocol != 'udp6') {
+        throw('\'protocol\' must be \'udp4\' or \'udp6\'.');
+    }
+    
+}
+
+
+//------ Inherit from 'events' module
+util.inherits(jsd, event);
+
+
+//------ Starts listening and sending packets to multicast address 
+jsd.prototype.start = function() {
+    
+    init(this);
+    
+    this.socket.bind({
+        port: port,
+        exclusive: true
+    });
+
+    timer.start();
+    
+}
+
+
+//------ Stops listening and sending packets 
+jsd.prototype.stop = function() {
+    
+    this.socket.close();
+    timer.stop();
+    timer.removeAllListeners('check');
+    hosts = [];
+    
+}
+
+
+//------ Returns active members
+jsd.prototype.hosts = function() {
+    
+    return hosts;
+    
+}
+
+
+//----------------------------------------- EXPORT MODULE
+module.exports = jsd;
